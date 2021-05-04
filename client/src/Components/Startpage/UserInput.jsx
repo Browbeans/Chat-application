e9314691c24f9ee92e59b2585bf48f3665e60efd
Petter <petter.fogel@medieinstitutet.se>
2021-05-04 15:11:37 +0200

    Add userInput component to handle socket logic

--- client/src/Components/Startpage/UserInput.jsx
+++ client/src/Components/Startpage/UserInput.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import socketIOClient from "socket.io-client";
+
+const ENDPOINT = "http://localhost:5000";
+
+var connectionOptions =  {
+    "force new connection" : true,
+    "reconnectionAttempts": "Infinity", 
+    "timeout" : 10000,                  
+    "transports" : ["websocket"]
+  };
 
 function UserInput() {
+    const [username, setUsername] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const socket = socketIOClient(ENDPOINT, connectionOptions);
+        socket.emit('username', username);
+    }
+
     return (
-        <h1>UserInput</h1>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    placeholder="Your Name"
+                    required
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <input type="submit" value="Send"/>
+            </form>
+        </div>
     )
 }
 
